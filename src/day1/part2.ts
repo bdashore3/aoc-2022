058ca3cc5a4a3f5cdd4e2fbfa2bf61cc0d48ca0a
kingbri <bdashore3@proton.me>
2023-01-05 13:58:44 -0500

    Day 1
    Signed-off-by: kingbri <bdashore3@proton.me>

---
+++ src/day1/part2.ts
@@ -0,0 +1,21 @@
+import { promises as fs } from 'fs';
+
+try {
+    await main();
+} catch (err) {
+    console.log(`Uncaught exception: \n\n${err}`);
+}
+
+async function main() {
+    const data = await fs.readFile('input/day1.txt', 'utf-8');
+    const splitData = data.split('\n\n');
+    const addedSplitData = splitData.map((e) => {
+        const splitElement = e.split('\n').map((e) => parseInt(e));
+        const sumOfElements = splitElement.reduce((partialSum, e) => partialSum + e, 0);
+        return sumOfElements;
+    });
+
+    const sorted = addedSplitData.sort((a, b) => a - b).reverse();
+
+    console.log(sorted[0] + sorted[1] + sorted[2]);
+}
